f8de69cadde98cb766d3d919aae503ec8644d549
51ding <houhanbin121456@163.com>
2018-11-22 22:59:27 +0800

    重构代码

--- test/message.test.js
+++ test/message.test.js
@@ -4,5 +4,5 @@ var Message =require("../wechat/message");
 var message = new Message();
 
 message.text("1",async functon(ctx,next){
-
+	
 })
---
+++ wechat/message/index.js
@@ -0,0 +1,4 @@
+class Message{
+}
+
+["text","video",""]
