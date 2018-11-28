57b9ebf4829dea6b8dca6de87296b9eadb1bc3b6
51ding <houhanbin121456@163.com>
2018-11-28 23:47:54 +0800

    代码重构

---
+++ test/app.js
@@ -0,0 +1,19 @@
+var Koa=require("koa");
+
+var WeChat =require("../lib");
+
+var app=new Koa();
+
+var options= {
+	appid:"",
+	appsecret:"",
+	token:"",
+	issafe:false
+};
+
+app.use(WeChat(options))
+
+
+app.listen(3000,()=>{
+	console.log("server is running!");
+})
